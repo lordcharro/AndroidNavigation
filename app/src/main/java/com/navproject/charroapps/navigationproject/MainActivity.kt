3268132ffa51e7c8e42a237ec589457b1184425b
ilopes <ilopes@openwt.com>
2018-10-16 17:25:50 +0200

    Initial commit

---
+++ app/src/main/java/com/navproject/charroapps/navigationproject/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.navproject.charroapps.navigationproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(), FirstFragment.OnFragmentInteractionListener, SecondFragment.OnFragmentInteractionListener  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onFragmentInteractionFragment1(string: String) {
+        // Do stuff
+    }
+
+    override fun onFragmentInteractionFragment2(string: String) {
+        // Do stuff
+    }
+}
